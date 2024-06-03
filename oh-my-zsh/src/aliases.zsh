492e8dd4fbad8699a7b303eae8eac760182ca1c2
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-03 14:18:46 -0700

    Load aliases from oh-my-john

--- oh-my-zsh/src/aliases.zsh
+++ oh-my-zsh/src/aliases.zsh
@@ -1,12 +1,12 @@
 # Load main aliases first
-source ~/.oh-my-zsh/custom/main/main.zsh
+source ~/.oh-my-john/custom/main/main.zsh
 
 # Load generalized aliases
-for file in ~/.oh-my-zsh/custom/aliases/*; do
+for file in ~/.oh-my-john/custom/aliases/*; do
   source $file
 done
 
 # Load local aliases
-for file in ~/.oh-my-zsh/custom/local_aliases/*; do
+for file in ~/.oh-my-john/custom/local_aliases/*; do
   source $file
 done
