b6743563fc81a8ead6da0f6984acd772b7f08e3c
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-03 14:27:38 -0700

    Update warning

--- oh-my-zsh/setup.zsh
+++ oh-my-zsh/setup.zsh
@@ -5,4 +5,4 @@ cp -r ./src/* ~/.oh-my-john
 mv ~/.oh-my-john/aliases.zsh ~/.oh-my-zsh/custom
 source ~/.zshrc
 
-echo "Alias setup complete. Remember to change ~/.oh-my-john/local_aliases to match local env."
+echo "Alias setup complete. Remember to change ~/.oh-my-john/main/local_aliases.zsh to match local env."
