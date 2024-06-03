c0f9f85cc29d4023dc4dd3b6457a1310ff6a652d
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-03 14:20:13 -0700

    Remove custom

--- oh-my-zsh/src/aliases.zsh
+++ oh-my-zsh/src/aliases.zsh
@@ -1,12 +1,12 @@
 # Load main aliases first
-source ~/.oh-my-john/custom/main/main.zsh
+source ~/.oh-my-john/main/main.zsh
 
 # Load generalized aliases
-for file in ~/.oh-my-john/custom/aliases/*; do
+for file in ~/.oh-my-john/aliases/*; do
   source $file
 done
 
 # Load local aliases
-for file in ~/.oh-my-john/custom/local_aliases/*; do
+for file in ~/.oh-my-john/local_aliases/*; do
   source $file
 done
