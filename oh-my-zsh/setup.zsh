ecd9583f3508fb30ae2f51bb6601cd0e35d400cd
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-03 14:26:00 -0700

    Warning message for local aliases

--- oh-my-zsh/setup.zsh
+++ oh-my-zsh/setup.zsh
@@ -4,3 +4,5 @@ mkdir -p ~/.oh-my-john
 cp -r ./src/* ~/.oh-my-john
 mv ~/.oh-my-john/aliases.zsh ~/.oh-my-zsh/custom
 source ~/.zshrc
+
+echo "Alias setup complete. Remember to change ~/.oh-my-john/local_aliases to match local env."
