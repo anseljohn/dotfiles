466c896fe649773d72c3e94b60303d28df0bf86e
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-05 16:45:27 -0700

    Compinit

--- oh-my-zsh/setup.zsh
+++ oh-my-zsh/setup.zsh
@@ -3,6 +3,6 @@
 mkdir -p ~/.oh-my-john
 cp -r ./src/* ~/.oh-my-john
 mv ~/.oh-my-john/aliases.zsh ~/.oh-my-zsh/custom
-exec zsh
+compinit
 
 echo "Alias setup complete. Remember to change ~/.oh-my-john/main/local_aliases.zsh to match local env."
