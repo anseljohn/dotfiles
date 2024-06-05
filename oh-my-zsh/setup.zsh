a2f3be7d13ed00e54f7944bec52881ff8f4c3c9c
John Anselmo <johnanselmo@nianticlabs.com>
2024-06-05 16:42:44 -0700

    Exec not source

--- oh-my-zsh/setup.zsh
+++ oh-my-zsh/setup.zsh
@@ -3,7 +3,6 @@
 mkdir -p ~/.oh-my-john
 cp -r ./src/* ~/.oh-my-john
 mv ~/.oh-my-john/aliases.zsh ~/.oh-my-zsh/custom
-source ~/.zshrc
 exec zsh
 
 echo "Alias setup complete. Remember to change ~/.oh-my-john/main/local_aliases.zsh to match local env."
