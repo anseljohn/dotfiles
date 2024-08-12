7c998dc4188bd37b99b20b4ddeb1a8444c18485e
John Anselmo <johnanselmo@nianticlabs.com>
2024-08-12 14:10:30 -0700

    Update aliases

--- oh-my-zsh/src/aliases/project_aliases.zsh
+++ oh-my-zsh/src/aliases/project_aliases.zsh
@@ -241,9 +241,9 @@ copyDepths() {
   if [ $# -lt 2 -o $# -gt 2 ];
   then
     err "Invalid number of arguments."
-    echo "Syntax: copyDepths <path/to/render> <path/to/recorderV2/folder>"
+    echo "Syntax: copyDepths <path/to/RenderSplatsOutput> <path/to/RecorderV2Scan>"
   else
-    python3 ~/dev/tools/SplatDepthToRV2/SplatDepthToRV2.py $1 $2
+    python3 $MASSF/tools/scripts/meshing_tools/copy_splat_depths_to_v2.py $1 $2
   fi
 
 }
